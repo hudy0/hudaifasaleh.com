f84cfa24d898eaa2bda9a40204e741c746aa8ab2
hudy0 <hudaifa021@icloud.com>
2024-06-18 01:58:29 +0300

    starting a django project

---
+++ project/envs.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, []),
+)
+env.read_env(str(BASE_DIR / "project/.env"))
