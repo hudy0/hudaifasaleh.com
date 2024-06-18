398db3df764a2ce7ffd3317679ee3bedbc738e77
hudy0 <hudaifa021@icloud.com>
2024-06-19 00:55:59 +0300

    static files

--- project/urls.py
+++ project/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-                  path("accounts/", include("allauth.urls")),
-                  path(settings.DJANGO_ADMIN_URL, admin.site.urls),
-                  path("", include("django_blog.home.urls")),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    path("accounts/", include("allauth.urls")),
+    path(settings.DJANGO_ADMIN_URL, admin.site.urls),
+    path("", include("django_blog.home.urls")),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
