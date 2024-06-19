6f01c5896d1acc25bef5f9601b7e761156159af4
hudy0 <hudaifa021@icloud.com>
2024-06-19 03:38:50 +0300

    Add refactor urls

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ URL configuration for project.
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
