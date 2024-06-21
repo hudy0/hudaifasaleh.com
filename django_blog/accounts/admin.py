e65fb73bbd46d855aaff5434bc851485454c77b2
hudy0 <hudaifa021@icloud.com>
2024-06-21 23:59:05 +0300

    Add django admin user to display fields

--- django_blog/accounts/admin.py
+++ django_blog/accounts/admin.py
@@ -4,9 +4,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-# admin.site.register(User, UserAdmin)
-
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ("username", "email", "first_name", "last_name", "is_active", "is_staff")
