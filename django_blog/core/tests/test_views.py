f44bc13de4059f1da9872a872d2c5b201e1e5dba
hudy0 <hudaifa15@outlook.com>
2024-06-26 01:54:25 +0300

    Add test for terms and privacy

--- django_blog/core/tests/test_views.py
+++ django_blog/core/tests/test_views.py
@@ -4,19 +4,19 @@ from django.urls import reverse
 class TestIndex:
     def test_unauthenticated(self, client):
         """An unauthenticated user gets a valid response."""
-        response = client.get(reverse("core:index"))
+        response = client.get(reverse("index"))
         assert response.status_code == 200
 
 
 class TestTerms:
-    def test_terms_of_service(self, client):
+    def test_terms(self, client):
         """Test, The terms of service page of the application."""
-        response = client.get(reverse("core:terms"))
+        response = client.get(reverse("terms"))
         assert response.status_code == 200
 
 
 class TestPrivacy:
-    def test_terms_of_service(self, client):
+    def test_privacy(self, client):
         """Test, The privacy of service page of the application."""
-        response = client.get(reverse("core:privacy"))
+        response = client.get(reverse("privacy"))
         assert response.status_code == 200
