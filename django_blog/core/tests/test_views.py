bcb5ee44a6b1d38ade39f03dac89a51eb9931993
hudy0 <hudaifa15@outlook.com>
2024-06-26 04:57:09 +0300

    add 100% test for index class and format file

--- django_blog/core/tests/test_views.py
+++ django_blog/core/tests/test_views.py
@@ -1,9 +1,17 @@
 from django.urls import reverse
 
+from django_blog.accounts.tests.factories import UserFactory
+
 
 class TestIndex:
     def test_unauthenticated(self, client):
         """An unauthenticated user gets a valid response."""
+        response = client.get(reverse("index"))
+        assert response.status_code == 200
+
+    def test_authenticated(self, client):
+        """An authenticated user gets a valid response."""
+        client.force_login(UserFactory())
         response = client.get(reverse("index"))
         assert response.status_code == 200
 
